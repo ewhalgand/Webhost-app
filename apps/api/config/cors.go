c07d2cb2ac2bb304971d68186e8a0e2ada285994
ewhalgand <halgandewen44@gmail.com>
2025-07-24 20:58:27 +0200

    fix: cors origins & headers

--- apps/api/config/cors.go
+++ apps/api/config/cors.go
@@ -4,9 +4,9 @@ import "github.com/gin-contrib/cors"
 
 func CorsConfig() cors.Config {
 	return cors.Config{
-		AllowOrigins:     []string{"http://localhost:4000"},
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
 		AllowCredentials: true,
 	}
 }
